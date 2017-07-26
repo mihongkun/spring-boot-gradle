b3f29bfcf7322cae7c8c4d2f6b2338e906cccca6
mihk <mihongkun@gmail.com>
2017-07-26 20:20:37 +0800

    something

---
+++ mpc/src/main/java/com/mihk/mpc/MpcApplication.java
@@ -0,0 +1,12 @@
+package com.mihk.mpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpcApplication.class, args);
+	}
+}
